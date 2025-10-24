a65cc02b3dfe29c4fdfa35355448e637cb2074a0
Sarthak Khanna <147915708+Sarthakkhanna07@users.noreply.github.com>
2025-10-25 01:07:19 +0530

    Time: 569 ms (20.18%) | Memory: 254.9 MB (24.22%) - GitYourDSA

--- 2174-next-greater-numerically-balanced-number/next-greater-numerically-balanced-number.cpp
+++ 2174-next-greater-numerically-balanced-number/next-greater-numerically-balanced-number.cpp
@@ -1,35 +1,30 @@
 class Solution {
-private:
-    // A static set to store all balanced numbers, initialized once.
-    static set<long long> balancedNumbers;
-
-    static set<long long> initBalancedNumbers() {
-        set<long long> s;
-
-        vector<string> bases = {
-            "1", "22", "122", "333", "1333", "4444", "14444", "22333", "55555",
-            "122333", "155555", "224444", "666666", "1224444", "1666666", 
-            "2255555", "3334444", "7777777", 
-            "12255555"
-        };
-
-        for (string base : bases) {
-            do {
-                // Convert the permutation string to long long and insert into the set
-                s.insert(stoll(base)); 
-            } while (next_permutation(base.begin(), base.end()));
+public:
+    bool balanced(int num) {
+        vector<int> freq(10);
+        while(num > 0) {
+            int digit = num%10;
+            freq[digit]++;
+            num /= 10;
         }
-        return s;
+
+        for(int d = 0; d < 10; d++) {
+            if(freq[d] != 0 && freq[d] != d)
+                return false;
+        }
+        
+        return true;
     }
 
-public:
     int nextBeautifulNumber(int n) {
-        // Find the first element in the set that is strictly greater than n
-        auto it = balancedNumbers.upper_bound(n);
-
-        return (it != balancedNumbers.end()) ? *it : -1;
+        for(int num = n+1; num <= 1224444; num++) {
+            if(balanced(num)) {
+                return num;
+            }
+        }
+        return -1;
     }
 };
 
-// Initialize the static member variable outside the class
-set<long long> Solution::balancedNumbers = Solution::initBalancedNumbers();
+//T.C : O(L-n), where L = 1224444
+//S.C : O(1)
